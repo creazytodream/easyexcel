7aee79165bb0e336e35549152a92120682f48f31
Jerry <1076950448@qq.com>
2019-01-09 15:39:07 +0800

    添加java生成二维码工具类

--- src/test/java/com/example/excel/ExcelApplicationTests.java
+++ src/test/java/com/example/excel/ExcelApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.excel;
 
+import com.example.excel.excel.QRCodeUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,5 +14,25 @@ public class ExcelApplicationTests {
     public void contextLoads() {
     }
 
+
+    @Test
+    public void testQrCode() {
+        String text = "hello world!"; // 随机生成验证码
+        System.out.println("随机码： " + text);
+        int width = 100; // 二维码图片的宽
+        int height = 100; // 二维码图片的高
+        String format = "png"; // 二维码图片的格式
+
+        try {
+            // 生成二维码图片，并返回图片路径
+            String pathName = QRCodeUtil.generateQRCode(text, width, height, format, "/Users/yumingbin/Pictures/qrcode.png");
+            System.out.println("生成二维码的图片路径： " + pathName);
+
+            String content = QRCodeUtil.parseQRCode(pathName);
+            System.out.println("解析出二维码的图片的内容为： " + content);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
